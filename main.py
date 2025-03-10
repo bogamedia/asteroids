6c3539d9caf08fa6891b9f4f70b3125a395ec8a0
bogamedia <code@bogamedia.com>
2025-03-09 21:27:12 -0600

    CH2: Player. L2: Game Loop. Fixed window persistence.

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import pygame
 from constants import *
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-	while True:
-		screen.fill("black")
-		pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
-	main()
+    main()
