2552b4e80b306dfebb21101ba85325a2d6181246
bogamedia <code@bogamedia.com>
2025-03-10 22:06:15 -0600

    CH2: Player. L2: Draw Player. V-02.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     fps = pygame.time.Clock()
     dt = 0
 
@@ -17,6 +18,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         dt = fps.tick(60) / 1000
